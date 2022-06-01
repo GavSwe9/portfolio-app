96f9a67d7c0f78928057e7ab647816303cd7258d
Gavin Sweeney <gavswe19@gmail.com>
2022-06-01 19:30:29 -0400

    Adjust margins for section headers

--- src/Components/HomeComponents/Certifications.js
+++ src/Components/HomeComponents/Certifications.js
@@ -9,7 +9,7 @@ import { ZedExchange } from './ProjectComponents/ZedExchange'
 export function Certifications() {
     return (
         <div>
-            <div className="w-64 mx-auto my-4 rounded-full text-lg text-gray-700 font-semibold bg-gray-200 select-none">
+            <div className="w-64 mx-auto mb-4 mt-8 rounded-full text-lg text-gray-700 font-semibold bg-gray-200 select-none">
                 Certifications
             </div>
             <div className="flex justify-evenly flex-wrap">
--- src/Components/HomeComponents/Projects.js
+++ src/Components/HomeComponents/Projects.js
@@ -8,7 +8,7 @@ import { ZedExchange } from './ProjectComponents/ZedExchange'
 export function Projects() {
     return (
         <div>
-            <div className="w-64 mx-auto my-4 rounded-full text-lg text-gray-700 font-semibold bg-gray-200 select-none">
+            <div className="w-64 mx-auto mb-4 mt-8 rounded-full text-lg text-gray-700 font-semibold bg-gray-200 select-none">
                 Links & Projects
             </div>
             <div className="flex justify-evenly flex-wrap">
