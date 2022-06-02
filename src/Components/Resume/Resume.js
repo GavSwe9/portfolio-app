ffc3bf4b6348359ce1d3bdc254ce94a6440d7df2
Gavin Sweeney <gavswe19@gmail.com>
2022-06-01 20:46:40 -0400

    Update resume location

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -3,7 +3,7 @@ import React from 'react';
 export function Resume() {
     return (
         <div className="w-screen h-screen">
-            <object className="w-full h-full block" data="https://gavswe-resume.s3.amazonaws.com/Gavin_Sweeney_Resume.pdf" type="application/pdf"></object>
+            <object className="w-full h-full block" data="https://gavswe-resume-site.s3.amazonaws.com/GavinSweeneyResume.pdf" type="application/pdf"></object>
         </div>
     )
 }
